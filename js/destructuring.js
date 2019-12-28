564440bdade4e7392c75c1bf14c0f78b08316eee
Brian Kim <briaansjkim@gmail.com>
2019-12-28 18:15:22 -0500

    practice w/ object destructuring

--- js/destructuring.js
+++ js/destructuring.js
@@ -2,4 +2,17 @@
 const numbers = [1, 2, 3];
 [num1, num2] = numbers;
 
-console.log(num1, num2);
+// console.log(num1, num2);
+
+// object destructuring
+const person = { name: 'Brian', age: 23};
+const { name, age } = person;
+
+// console.log(name);
+// console.log(age);
+
+// assignment without declaration
+const child = { nickName: 'B' };
+({nickName} = child)
+
+// console.log(nickName);
