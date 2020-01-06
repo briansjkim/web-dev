76bb36be01382a1dc7001f1f9a02b79c7bb43c0d
Brian Kim <briaansjkim@gmail.com>
2020-01-06 14:59:19 -0500

    added another example using prototypal inheritance

--- js/protoInheritance.js
+++ js/protoInheritance.js
@@ -16,3 +16,18 @@ var batman = new Hero("Batman", "Bruce Wayne", "Earth");
 // when it doesn't find one, it'll look up the prototype chain to batman's parent--Hero.prototype
 // There, it'll find Hero.prototype.dialogue and calls it with a 'this' which is bound to batman
 batman.dialogue();
+
+// another way to do what we did in lines 14-18
+function Batman() {
+  Hero.call(this);
+  this.name = "Batman";
+  this.alias = "Bruce Wayne";
+  this.planet = "Earth";
+}
+
+Batman.prototype = Object.create(Hero.prototype);
+Batman.prototype.constructor = Batman;
+
+var bat = new Batman();
+
+bat.dialogue();
