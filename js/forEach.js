c0c47e6d8c471ce4b25931f1e3cbf1e7b12786af
Brian Kim <briaansjkim@gmail.com>
2020-01-23 17:42:26 -0500

    more practice w/ forEach

--- js/forEach.js
+++ js/forEach.js
@@ -5,3 +5,9 @@ var suspects = ['Brian', 'Josh', 'Austin'];
 suspects.forEach((suspect) => {
   console.log(suspect); // logs each name one by one
 });
+
+suspects.forEach((suspect) => {
+  suspects.push('Boo'); // pushes 'Boo' at the end for every suspect in suspects
+})
+
+console.log(suspects);
