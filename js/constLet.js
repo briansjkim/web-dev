de074e8ecad0c7d703e4e300776d070fde8e1cd6
Brian Kim <briaansjkim@gmail.com>
2020-01-23 17:24:19 -0500

    practice with let

--- js/constLet.js
+++ js/constLet.js
@@ -9,3 +9,15 @@ b = 14;
 
 // console.log(a); // 10
 // console.log(b); // TypeError: Assignment to constant variable
+
+
+// 'let' is also block-scoped
+
+let x = 1;
+
+if (x === 1) {
+  let x = 5;
+  console.log(x); // 5
+}
+
+console.log(x); // 1
