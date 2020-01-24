460647706efbb3fbc42ce571a9d21d2bd4c66e2a
Brian Kim <briaansjkim@gmail.com>
2020-01-24 17:03:39 -0500

    init set up for practice problem

---
+++ js/projecting.js
@@ -0,0 +1,29 @@
+const videoData = [
+  {
+    name: 'Miss Scarlet',
+    present: true,
+    rooms: [
+      { kitchen: false },
+      { ballroom: false },
+      { conservatory: false },
+      { 'dining room': false },
+      { 'billiard room': false },
+      { library: false }
+    ]
+  },
+  {
+    name: 'Mrs. White',
+    present: false,
+    rooms: [
+      { kitchen: false },
+      { ballroom: false },
+      { conservatory: false },
+      { 'dining room': false },
+      { 'billiard room': false },
+      { library: false }
+    ]
+  }
+]
+
+// Filter and then map this data structure to get the names of the final susepcts
+
