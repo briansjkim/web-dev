abf6f09b2b6ee6154c3a11315552a1f312d42ed0
Brian Kim <briaansjkim@gmail.com>
2020-01-29 14:34:38 -0500

    more practice w/ currying

--- js/currying.js
+++ js/currying.js
@@ -8,3 +8,24 @@ var addTwo = function (a) {
 
 var one = addTwo(1);
 // console.log(one(2));
+
+function createFunction() {
+  function multiplyBy2(num) {
+    return num * 2;
+  }
+  return multiplyBy2;
+}
+
+function createFunction2() {
+  return function (num) {
+    return num * 2;
+  }
+}
+
+var generatedFn = createFunction();
+var result = generatedFn(3);
+// console.log(result); //6
+
+var generatedFn2 = createFunction2();
+var result2 = generatedFn2(3);
+// console.log(result2); // 6
